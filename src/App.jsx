8d15b4f947141a1cfbdc9be40b64645ab364069f
LuluCake Admin <admin@tiembanhlulu.com>
2026-02-09 16:58:25 +0700

    Tool: Remove Background Remover as requested

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import TikTokSection from './components/TikTokSection';
 import ProductGallery from './components/ProductGallery';
 import AdminLogin from './components/AdminLogin';
 import AdminDashboard from './components/AdminDashboard';
-import BackgroundRemover from './components/BackgroundRemover';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
         } />
         <Route path="/AdminLulucake" element={<AdminLogin />} />
         <Route path="/AdminLulucake/dashboard" element={<AdminDashboard />} />
-        <Route path="/AdminLulucake/tools/bg-remover" element={<BackgroundRemover />} />
       </Routes>
     </BrowserRouter>
   );
